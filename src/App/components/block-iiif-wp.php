cb33759eea37bec576b805bee5b184b2fa71829e
Mat Jordan <mat@utk.edu>
2021-04-22 16:14:46 -0400

    Map ACF fields to viewer containers.

--- src/App/components/block-iiif-wp.php
+++ src/App/components/block-iiif-wp.php
@@ -17,14 +17,14 @@ if (is_array($context)) :
         $label = $manifest->label->en[0];
         $summary = $manifest->summary->en[0];
         $canvas = $manifest->items[0]->items[0]->items[0]->body->id;
-        $canvas = str_replace('/full/full', '/full/!1000,1000', $canvas);
+        $canvas = str_replace('/full/full', '/full/!640,640', $canvas);
     endif;
 else :
     $presentation = 2;
     $label = $manifest->label;
     $summary = $manifest->decription;
     $canvas = $manifest->sequences[0]->canvases[0]->images[0]->resource->{'@id'};
-    $canvas = str_replace('/full/full', '/full/1000,1000', $canvas);
+    $canvas = str_replace('/full/full', '/full/640,640', $canvas);
 endif;
 
 ?>
@@ -45,8 +45,10 @@ endif;
 
 <div class="iiif-wp"
      id="<?php print $block['id']; ?>">
-    <div class="iiif-wp-viewer">
-        <?php print $fields['viewer']; ?>
+    <div class="iiif-wp-viewer"
+         id="iiif-wp-viewer-<?php print str_replace('block_', '', $block['id']); ?>"
+         data-manifest="<?php print $fields['manifest']; ?>"
+         data-viewer="<?php print $fields['viewer']; ?>">
     </div>
     <?php if ($fields['mode'] === 'preview') : ?>
         <figure>
@@ -59,8 +61,8 @@ endif;
                 <?php endif; ?>
             </figcaption>
         </figure>
-        <div class="iiif-wp-actions">
-            <a href="#viewer">Expand in Viewer</a>
-        </div>
+<!--        <div class="iiif-wp-actions">-->
+<!--            <a href="#viewer">Expand in Viewer</a>-->
+<!--        </div>-->
     <?php endif; ?>
 </div>
