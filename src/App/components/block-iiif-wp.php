fbe9a8f52a3279b8769cd1022f3a0fcc6a8d8e73
Mat Jordan <mat@utk.edu>
2021-04-29 08:57:26 -0400

    Fix body array bug.

--- src/App/components/block-iiif-wp.php
+++ src/App/components/block-iiif-wp.php
@@ -18,7 +18,7 @@ if (is_array($context)) :
         $presentation = 3;
         $label = $manifest->label->en[0];
         $summary = $manifest->summary->en[0];
-        $canvas = $manifest->items[0]->items[0]->items[0]->body->id;
+        $canvas = $manifest->items[0]->items[0]->items[0]->body[0]->id;
         $preview = str_replace('/full/full', '/full/!640,640', $canvas);
     endif;
 else :
